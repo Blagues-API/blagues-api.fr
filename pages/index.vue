8650ac93b74bb7c9fb32262f5735fa46c930bf7e
DraftProducts <nicovanaarsen@gmail.com>
2021-12-03 18:00:27 +0100

    feat: ✨ Add home page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <HomeHero />
+    <HomeInformations :count="count" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const { count } = await $axios.$get(`/api/count`)
+    return { count }
+  },
+}
+</script>
