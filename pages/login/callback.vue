52c91f4e6ac5d96fc8d96e899fe702015d1821f4
DraftProducts <nicovanaarsen@gmail.com>
2021-12-06 01:25:12 +0100

    refactor: ♻️ Add a real callback page

--- pages/login/callback.vue
+++ pages/login/callback.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>cc</div>
+  <div class="callback"><LoginLoader /></div>
 </template>
+
+<style lang="scss">
+.callback {
+  display: flex;
+  justify-content: center;
+  min-height: calc(100vh - 96px - 40px);
+  padding-top: 64px;
+  background-color: var(--background);
+  span {
+    padding-top: 64px;
+  }
+  @media screen and(max-width: 720px) {
+    min-height: calc(100vh - 96px - 60px);
+  }
+}
+</style>
